b93ca7362d3e137bd838638b390718a88debf566
Christian Kusi <christian.kusi@st.vvu.edu.gh>
2024-07-19 14:46:19 +0000

    updating UI and adding notification emergency form

--- lib/notify.dart
+++ lib/notify.dart
@@ -1,7 +1,173 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+//
+// class Notify extends StatefulWidget {
+//   const Notify({super.key});
+//
+//   @override
+//   State<Notify> createState() => _NotifyState();
+// }
+//
+// class _NotifyState extends State<Notify> {
+//   final _formKey = GlobalKey<FormState>();
+//
+//   final TextEditingController _nameController = TextEditingController();
+//   final TextEditingController _emailController = TextEditingController();
+//   final TextEditingController _phoneController = TextEditingController();
+//   final TextEditingController _organizationController = TextEditingController();
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Emergency Report Form'),
+//         backgroundColor: const Color(0xFF004d40),
+//         centerTitle: true,
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(16.0),
+//         child: Form(
+//           key: _formKey,
+//           child: Column(
+//             children: [
+//               TextFormField(
+//                 controller: _nameController,
+//                 decoration: InputDecoration(
+//                   labelText: 'Name',
+//                   border: OutlineInputBorder(
+//                     borderRadius: BorderRadius.circular(15.0),
+//                   ),
+//                 ),
+//                 validator: (value) {
+//                   if (value == null || value.isEmpty) {
+//                     return 'Please enter your name';
+//                   }
+//                   return null;
+//                 },
+//               ),
+//               const SizedBox(height: 20),
+//               TextFormField(
+//                 controller: _emailController,
+//                 decoration: InputDecoration(
+//                   labelText: 'Email',
+//                   border: OutlineInputBorder(
+//                     borderRadius: BorderRadius.circular(15.0),
+//                   ),
+//                 ),
+//                 validator: (value) {
+//                   if (value == null || value.isEmpty) {
+//                     return 'Please enter your email';
+//                   }
+//                   return null;
+//                 },
+//               ),
+//               const SizedBox(height: 20),
+//               TextFormField(
+//                 controller: _phoneController,
+//                 decoration: InputDecoration(
+//                   labelText: 'Phone Number',
+//                   border: OutlineInputBorder(
+//                     borderRadius: BorderRadius.circular(15.0),
+//                   ),
+//                 ),
+//                 validator: (value) {
+//                   if (value == null || value.isEmpty) {
+//                     return 'Please enter your phone number';
+//                   }
+//                   return null;
+//                 },
+//               ),
+//               const SizedBox(height: 20),
+//               TextFormField(
+//                 controller: _organizationController,
+//                 decoration: InputDecoration(
+//                   labelText: 'Organization',
+//                   border: OutlineInputBorder(
+//                     borderRadius: BorderRadius.circular(15.0),
+//                   ),
+//                 ),
+//                 validator: (value) {
+//                   if (value == null || value.isEmpty) {
+//                     return 'Please enter your organization';
+//                   }
+//                   return null;
+//                 },
+//               ),
+//               const SizedBox(height: 40),
+//               ElevatedButton(
+//                 onPressed: () {
+//                   if (_formKey.currentState?.validate() ?? false) {
+//                     // Save the form data
+//                   }
+//                 },
+//                 style: ElevatedButton.styleFrom(
+//                   backgroundColor: const Color(0xFF004d40),
+//                   shape: RoundedRectangleBorder(
+//                     borderRadius: BorderRadius.circular(15.0),
+//                   ),
+//                   padding: const EdgeInsets.symmetric(vertical: 16.0),
+//                   minimumSize: const Size.fromHeight(50),
+//                 ),
+//                 child: const Text(
+//                   'Save',
+//                   style: TextStyle(fontSize: 16, color: Colors.white),
+//
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
-class Notify extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Notify extends StatefulWidget {
   const Notify({super.key});
+
+  @override
+  State<Notify> createState() => _NotifyState();
+}
+
+class _NotifyState extends State<Notify> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _organizationController = TextEditingController();
+  SharedPreferences? prefs;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserData();
+  }
+
+  Future<void> _loadUserData() async {
+    prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _nameController.text = prefs?.getString('name') ?? '';
+      _emailController.text = prefs?.getString('email') ?? '';
+      _phoneController.text = prefs?.getString('phone') ?? '';
+      _organizationController.text = prefs?.getString('organization') ?? '';
+    });
+  }
+
+  Future<void> _saveUserData() async {
+    if (_formKey.currentState?.validate() ?? false) {
+      if (prefs != null) {
+        await prefs!.setString('name', _nameController.text);
+        await prefs!.setString('email', _emailController.text);
+        await prefs!.setString('phone', _phoneController.text);
+        await prefs!.setString('organization', _organizationController.text);
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Information saved successfully')),
+        );
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,12 +176,93 @@ class Notify extends StatelessWidget {
         title: const Text('Notify'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'Notification Screen',
-          style: TextStyle(fontSize: 24),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                const Text(
+                  'Emergency Report Form',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                _buildTextField(
+                  controller: _nameController,
+                  labelText: 'Name',
+                  validator: (value) => value!.isEmpty ? 'Please enter your name' : null,
+                ),
+                const SizedBox(height: 20),
+                _buildTextField(
+                  controller: _emailController,
+                  labelText: 'Email',
+                  validator: (value) => value!.isEmpty ? 'Please enter your email' : null,
+                ),
+                const SizedBox(height: 20),
+                _buildTextField(
+                  controller: _phoneController,
+                  labelText: 'Phone Number',
+                  validator: (value) => value!.isEmpty ? 'Please enter your phone number' : null,
+                ),
+                const SizedBox(height: 20),
+                _buildTextField(
+                  controller: _organizationController,
+                  labelText: 'Organization',
+                  validator: (value) => value!.isEmpty ? 'Please enter your organization' : null,
+                ),
+                const SizedBox(height: 40),
+                SizedBox(
+                  width: double.infinity, // Make button wider
+                  child: ElevatedButton(
+                    onPressed: _saveUserData,
+                    child: const Text('Save'),
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: const Color(0xFF004d40),
+                      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                      textStyle: const TextStyle(fontSize: 16, color: Colors.white), // Set button text color to white
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
+    );
+  }
+
+  Widget _buildTextField({
+    required TextEditingController controller,
+    required String labelText,
+    required String? Function(String?) validator,
+  }) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: labelText,
+        labelStyle: const TextStyle(color: Colors.black), // Label text color
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        filled: true,
+        fillColor: const Color(0xFFF5F5F5),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: const BorderSide(
+            color: Color(0xFF004d40),
+            width: 2.0,
+          ),
+        ),
+      ),
+      cursorColor: const Color(0xFF004d40), // Set cursor color to deep green
+      style: const TextStyle(color: Color(0xFF004d40)), // Set text color to deep green when active
+      validator: validator,
     );
   }
 }

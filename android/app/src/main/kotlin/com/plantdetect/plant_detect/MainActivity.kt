ef35294dd3c58da814243ca4138cb052dbdd99f0
Christian Kusi <christian.kusi@st.vvu.edu.gh>
2024-07-28 16:01:12 +0000

    sending report.

--- android/app/src/main/kotlin/com/plantdetect/plant_detect/MainActivity.kt
+++ android/app/src/main/kotlin/com/plantdetect/plant_detect/MainActivity.kt
@@ -2,4 +2,5 @@ package com.plantdetect.plant_detect
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
--- lib/notify.dart
+++ lib/notify.dart
@@ -1,147 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
-//
-// class Notify extends StatefulWidget {
-//   const Notify({super.key});
-//
-//   @override
-//   State<Notify> createState() => _NotifyState();
-// }
-//
-// class _NotifyState extends State<Notify> {
-//   final _formKey = GlobalKey<FormState>();
-//   final TextEditingController _nameController = TextEditingController();
-//   final TextEditingController _emailController = TextEditingController();
-//   final TextEditingController _phoneController = TextEditingController();
-//   final TextEditingController _organizationController = TextEditingController();
-//   SharedPreferences? prefs;
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     _loadUserData();
-//   }
-//
-//   Future<void> _loadUserData() async {
-//     prefs = await SharedPreferences.getInstance();
-//     _nameController.text = prefs?.getString('name') ?? '';
-//     _emailController.text = prefs?.getString('email') ?? '';
-//     _phoneController.text = prefs?.getString('phone') ?? '';
-//     _organizationController.text = prefs?.getString('organization') ?? '';
-//   }
-//
-//   Future<void> _saveUserData() async {
-//     if (_formKey.currentState?.validate() ?? false) {
-//       prefs ??= await SharedPreferences.getInstance();
-//       await prefs!.setString('name', _nameController.text);
-//       await prefs!.setString('email', _emailController.text);
-//       await prefs!.setString('phone', _phoneController.text);
-//       await prefs!.setString('organization', _organizationController.text);
-//       ScaffoldMessenger.of(context).showSnackBar(
-//         const SnackBar(content: Text('Information saved successfully')),
-//       );
-//     }
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Notify'),
-//         centerTitle: true,
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: SingleChildScrollView(
-//           child: Form(
-//             key: _formKey,
-//             child: Column(
-//               children: [
-//                 const Text(
-//                   'Emergency Report Form',
-//                   style: TextStyle(
-//                     fontSize: 24,
-//                     fontWeight: FontWeight.bold,
-//                   ),
-//                 ),
-//                 const SizedBox(height: 20),
-//                 _buildTextField(
-//                   controller: _nameController,
-//                   labelText: 'Name',
-//                   validator: (value) => value!.isEmpty ? 'Please enter your name' : null,
-//                 ),
-//                 const SizedBox(height: 20),
-//                 _buildTextField(
-//                   controller: _emailController,
-//                   labelText: 'Email',
-//                   validator: (value) => value!.isEmpty ? 'Please enter your email' : null,
-//                 ),
-//                 const SizedBox(height: 20),
-//                 _buildTextField(
-//                   controller: _phoneController,
-//                   labelText: 'Phone Number',
-//                   validator: (value) => value!.isEmpty ? 'Please enter your phone number' : null,
-//                 ),
-//                 const SizedBox(height: 20),
-//                 _buildTextField(
-//                   controller: _organizationController,
-//                   labelText: 'Organization',
-//                   validator: (value) => value!.isEmpty ? 'Please enter your organization' : null,
-//                 ),
-//                 const SizedBox(height: 40),
-//                 SizedBox(
-//                   width: double.infinity, // Make button wider
-//                   child: ElevatedButton(
-//                     onPressed: _saveUserData,
-//                     child: const Text('Save'),
-//                     style: ElevatedButton.styleFrom(
-//                       foregroundColor: Colors.white,
-//                       backgroundColor: const Color(0xFF004d40),
-//                       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-//                       textStyle: const TextStyle(fontSize: 16, color: Colors.white), // Set button text color to white
-//                     ),
-//                   ),
-//                 ),
-//               ],
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-//
-//   Widget _buildTextField({
-//     required TextEditingController controller,
-//     required String labelText,
-//     required String? Function(String?) validator,
-//   }) {
-//     return TextFormField(
-//       controller: controller,
-//       decoration: InputDecoration(
-//         labelText: labelText,
-//         labelStyle: const TextStyle(color: Colors.black), // Label text color
-//         border: OutlineInputBorder(
-//           borderRadius: BorderRadius.circular(20),
-//         ),
-//         filled: true,
-//         fillColor: const Color(0xFFF5F5F5),
-//         contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-//         focusedBorder: OutlineInputBorder(
-//           borderRadius: BorderRadius.circular(20),
-//           borderSide: const BorderSide(
-//             color: Color(0xFF004d40),
-//             width: 2.0,
-//           ),
-//         ),
-//       ),
-//       cursorColor: const Color(0xFF004d40), // Set cursor color to deep green
-//       style: const TextStyle(color: Color(0xFF004d40)), // Set text color to deep green when active
-//       validator: validator,
-//     );
-//   }
-// }
-
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -254,13 +110,13 @@ class _NotifyState extends State<Notify> {
                   width: double.infinity, // Make button wider
                   child: ElevatedButton(
                     onPressed: _saveUserData,
-                    child: const Text('Save'),
                     style: ElevatedButton.styleFrom(
                       foregroundColor: Colors.white,
                       backgroundColor: const Color(0xFF004d40),
                       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
                       textStyle: const TextStyle(fontSize: 16, color: Colors.white), // Set button text color to white
                     ),
+                    child: const Text('Save'),
                   ),
                 ),
               ],
